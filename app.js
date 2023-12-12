e8ef72fae046bcfe023de42e1ff6429792c1775c
Kuntal Magee <112935590+Kuntalmajee2557@users.noreply.github.com>
2023-12-13 00:01:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const inputSlider = document.querySelector("[data-LengthSlider]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+const lengthDisplay = document.querySelector("[data-LengthNumber]");
+
+43.19
